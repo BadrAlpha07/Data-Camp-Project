e64287e8dfbd95da50b4ef5069df15b50b44ee69
Bastien B <55130739+Bastienbbil@users.noreply.github.com>
2021-01-09 16:01:46 +0100

    upload_scrapping_updated

--- scrapping_functions.py
+++ scrapping_functions.py
@@ -93,7 +93,78 @@ class Scrapper:
         df = pd.DataFrame(Influencers, columns=['Links to profile','Name','Activity','Country','Brand'])
         return df,  df['Links to profile'].tolist()
 
-    def connection_instagram(self, instagram_account_link, user_name, password):
+    def get_influencers_france(self):
+        """
+        Retrieving top 49 World Influencers in France
+        ---------
+        Input : local path to Chrome WebDriver
+        ---------
+        Outputs : 
+            df : DataFrame with influencers' related infos
+            df['Links to profile'].tolist() : list of the accounts' links
+        """
+        from selenium import webdriver
+        import time
+        browser = self.browser
+        user = self.user
+        pwd = self.pwd
+        driver_path =self.driver_path
+        url = "https://hypeauditor.com/top-instagram/all/france/"
+        browser.get(url)
+        time.sleep(1)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        browser.quit()
+
+        insta_acc_links_int=[]
+        names_int=[]
+        activity = []
+        country=[]
+        brand = []
+
+        base = 'https://www.instagram.com/'
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        }
+
+        s = requests.Session()
+        s.headers.update(headers)
+        r = s.get(url,allow_redirects= False)
+
+        soup_int = BeautifulSoup(r.text, 'html.parser')
+        inst_in = soup_int.find('div',attrs={'class':"page"})
+        inst_1 = inst_in.find('div',attrs={'class':"page__content"})
+        inst_2 = inst_1.find('div',attrs={'class':"tab"}).find('table',attrs={'class':"table"}).find('tbody',attrs={'class':"tbody"})
+        inst_2 = inst_1.find_all('tr',attrs={'class':"tr"})
+        for influencer in inst_2:
+            inf = influencer.find_all('td',attrs={'class':"td"})
+            info_1 = inf[2].find('div',attrs={'class':"contributor-wrap cont__info"}).find_all('a')
+            try:
+                info_2 = info_1[0].find('div',attrs={'class':"contributor__title"}).text
+            except: 
+                info_2 = info_1[0].find('div',attrs={'class':"contributor__name-content"}).text
+            names_int.append(info_2)
+            
+            info_2 = info_1[1]['href']
+            insta_acc_links_int.append(info_2)
+            activities = []
+            info_3 = inf[3].find_all('div',attrs={'class':"tag__content"})
+            for i in info_3: 
+                activities.append(i.text)
+            activity.append(activities)
+        from pandas import DataFrame
+        Influencers = {'Links to profile': insta_acc_links_int,
+                    'Name': names_int,
+                    'Activity': activity   
+                    }
+        df = pd.DataFrame(Influencers, columns=['Links to profile','Name','Activity'])
+        return df,  df['Links to profile'].tolist()
+
+    def connection_instagram(self, account_link, user_name, password):
         """
         Connecting to instagram and passing connection messages
         ---------
@@ -105,10 +176,12 @@ class Scrapper:
         ---------
         Outputs : None
         """
-        browser = self.browser
+        from selenium import webdriver
+
+        browser = browser = webdriver.Chrome(executable_path = self.driver_path)
         user = self.user
         pwd = self.pwd
-        browser.get(instagram_account_link)
+        browser.get(account_link)
         cookies_click = browser.find_element_by_xpath("//button[text()='Accepter']")
         cookies_click.click()
         time.sleep(2)
@@ -130,15 +203,10 @@ class Scrapper:
         login_button = browser.find_element_by_xpath("//button[@type='submit']")
         time.sleep(2)
         login_button.click()
-
         time.sleep(2)
-
-        pass_click = browser.find_element_by_xpath("//button[text()='Plus tard']")
-        pass_click.click()
-        time.sleep(1)
         return browser
 
-    def get_influencer_posts(self,browser, account_link, pause_time):
+    def get_influencer_posts(self, df_infl, index_infl, browser, account_link, pause_time, user_name, password):
         """
         Get links of an influencer's posts and general information about the account
         ---------
@@ -149,10 +217,9 @@ class Scrapper:
         Outputs : 
             df : DataFrame containing the infos (posts links and account general infos)
         """
-        browser = browser
-        user = self.user
-        pwd = self.pwd
-        driver_path = self.driver_path
+        insta_acc_links_int = df_infl['Links to profile'].tolist()
+        names_int = df_infl['Name'].tolist()
+        activity = df_infl['Activity'].tolist()
 
         lien_pub = []
         liens=[]
@@ -171,18 +238,18 @@ class Scrapper:
         
         browser.get(account_link)
 
-        last_height = browser.execute_script("return document.body.scrollHeight")
-        while True:
+        #last_height = browser.execute_script("return document.body.scrollHeight")
+        for i in range(49):
 
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 
             time.sleep(pause_time)
 
-            new_height = browser.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
+            #new_height = browser.execute_script("return document.body.scrollHeight")
+            #if new_height == last_height:
+            #    break
+            #last_height = new_height
 
             time.sleep(2)
             soup = BeautifulSoup(browser.page_source, 'html.parser')
@@ -195,45 +262,43 @@ class Scrapper:
                     p = p['href']
                     publis.append(base + p)
         publis_2 = list(np.unique(publis))
-        print(account_link)
         print(len(publis_2))
+        print("Lien du compte:" + str(account_link))
         time.sleep(10)
         browser.get(account_link)
 
         try:
-            biography_1 = browser.find_element_by_xpath('html/body/div/section/main/div/header/section/div[2]/h1').text
+            biography_1 = str(browser.find_element_by_xpath('html/body/div/section/main/div/header/section/div[2]/h1').text)
         except:
-            biography_1 = ""
+            biography_1 = str(" ")
         try:
-            biography_2 = browser.find_element_by_xpath('html/body/div/section/main/div/header/section/div[2]/span').text
+            biography_2 = str(browser.find_element_by_xpath('html/body/div/section/main/div/header/section/div[2]/span').text)
         except:
-            biography_2 =""
-        biography = biography_1 + biography_2
-        print(biography)
+            biography_2 = str(" ")
+        biography = biography_1 + str(" ") + biography_2
+        print("Biographie:" + str(biography))
 
         number_of_posts = browser.find_element_by_xpath('html/body/div/section/main/div/header/section/ul/li/span').text
-        print(number_of_posts)
+        print("Nombre de posts:"+ str(number_of_posts))
 
         number_of_followers = browser.find_element_by_xpath('html/body/div/section/main/div/header/section/ul/li[2]/a/span').get_attribute('title')
-        print(number_of_followers)
+        print("Nombre de followers:"+ str(number_of_followers))
 
         try : 
             number_of_followings = browser.find_element_by_xpath('html/body/div/section/main/div/header/section/ul/li[3]/a/span').text
         except : 
             number_of_followings = 0
-        print(number_of_followings)
+        print("Nombre d'abonnement:" + str(number_of_followings))
 
         is_verified = browser.find_element_by_xpath('html/body/div/section/main/div/header/section/div/div/span').text
-        print(is_verified)
+        print("Statut:" + str(is_verified))
 
         time.sleep(2)
         for pub in publis_2:
             lien_pub.append(pub)
-            liens.append(insta_acc_links_int[i])
-            noms.append(names_int[i])
-            activité.append(activity[i])
-            pays.append(country[i])
-            marques.append(brand[i])
+            liens.append(insta_acc_links_int[index_infl])
+            noms.append(names_int[index_infl])
+            activité.append(activity[index_infl])
 
             biographys.append(biography)
             nb_posts.append(number_of_posts)
@@ -247,8 +312,6 @@ class Scrapper:
                     'Links to publication' : lien_pub,
                         'Name Account/Influencer': noms,
                         'Professional Activity': activité,
-                        'Nationality': pays,
-                        'Brand': marques,
                         'Account description':  biographys,
                         'Number of posts':  nb_posts,
                         'Number of followers':  followers,
@@ -256,60 +319,80 @@ class Scrapper:
                         'Verified Status': verif_st,        
                         }
         df = DataFrame(Influencers, columns= ['Links to profile','Links to publication' , 'Name Account/Influencer','Professional Activity',
-                                                'Nationality','Brand','Account description','Number of posts','Number of followers',
+                                                'Account description','Number of posts','Number of followers',
                                                 'Number of followings','Verified Status'])
-
+        browser.quit()
         return df
 
-    def get_pub_infos(self, pub_list, driver_path, instagram_account_link, user_name, password): 
-        """
-        Retrieving informations for each posts of a list of publications
-        ---------
-        Input : 
-            pub_list : List of publications' links
-            driver_path : local path to Chrome WebDriver
-            instagram_account_link : the link to an instagram account
-            user_name : user name of the account used to scrape
-            password : password of the account used to scrape
-        ---------
-        Outputs : 
-            df : DataFrame of the collected informations for each post
-        """
-        browser = self.browser
-        user = self.user
-        pwd = self.pwd
+def get_pub_infos(scrapper, pub_list, instagram_account_link, user_name, password): 
+    """
+    Retrieving informations for each posts of a list of publications
+    ---------
+    Input : 
+        pub_list : List of publications' links
+        driver_path : local path to Chrome WebDriver
+        instagram_account_link : the link to an instagram account
+        user_name : user name of the account used to scrape
+        password : password of the account used to scrape
+    ---------
+    Outputs : 
+        df : DataFrame of the collected informations for each post
+    """
+    browser = scrapper.connection_instagram(instagram_account_link, user_name, password)
+
+    caption = []
+    number_of_com = []
+    number_of_lik = []
+    dates = []
+    typ = []
+    l = []
+
+    for pub in pub_list: 
         
-        connection_instagram(driver_path, instagram_account_link, user_name, password)
-
-        caption = []
-        number_of_com = []
-        number_of_lik = []
-        dates = []
-        typ = []
-        l = []
-
-        for pub in pub_list: 
-            try:
+        try:
+            browser.get(pub)
+            time.sleep(4)
+        except:
+            browser.quit()
+            browser = scrapper.connection_instagram(instagram_account_link, user_name, password)
+            browser.get(pub)
+            time.sleep(4)
+        
+        try:
+            date = browser.find_element_by_xpath('/html/body/div/section/main/div/div/article/div[3]/div[2]/a/time').get_attribute('datetime')
+        except:
+            time.sleep(10)
+            try : 
                 browser.get(pub)
-            except:
-                browser.quit()
-                connection_instagram(driver_path, instagram_account_link, user_name, password)
-                browser.get(pub)
-            
-            try:
                 date = browser.find_element_by_xpath('/html/body/div/section/main/div/div/article/div[3]/div[2]/a/time').get_attribute('datetime')
-            except:
-                time.sleep(10)
-                try : 
-                    browser.get(pub)
-                    date = browser.find_element_by_xpath('/html/body/div/section/main/div/div/article/div[3]/div[2]/a/time').get_attribute('datetime')
-                except : 
-                    browser.quit()
-                    connection_instagram(driver_path, instagram_account_link, user_name, password)
-                    browser.get(pub)
-                    date = browser.find_element_by_xpath('/html/body/div/section/main/div/div/article/div[3]/div[2]/a/time').get_attribute('datetime')
-            
+            except : 
+                browser.quit()
+                browser = scrapper.connection_instagram(instagram_account_link, user_name, password)
+                browser.get(pub)
+                date = browser.find_element_by_xpath('/html/body/div/section/main/div/div/article/div[3]/div[2]/a/time').get_attribute('datetime')
+        
+        try:
             try:
+                nb_likes = browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/div/button/span').text
+                typ.append("Photo")
+
+            except: 
+                nb_likes= browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/span/span').text
+                typ.append("Video")
+        except:
+            time.sleep(6)
+            try : 
+                browser.get(pub)
+                try:
+                    nb_likes = browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/div/button/span').text
+                    typ.append("Photo")
+                except: 
+                    nb_likes= browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/span/span').text
+                    typ.append("Video")
+            except : 
+                browser.quit()
+                browser = scrapper.connection_instagram(instagram_account_link, user_name, password)
+                browser.get(pub)
                 try:
                     nb_likes = browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/div/button/span').text
                     typ.append("Photo")
@@ -317,59 +400,39 @@ class Scrapper:
                 except: 
                     nb_likes= browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/span/span').text
                     typ.append("Video")
-            except:
-                time.sleep(6)
-                try : 
-                    browser.get(pub)
-                    try:
-                        nb_likes = browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/div/button/span').text
-                        typ.append("Photo")
-                    except: 
-                        nb_likes= browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/span/span').text
-                        typ.append("Video")
-                except : 
-                    browser.quit()
-                    connection_instagram(driver_path, instagram_account_link, user_name, password)
-                    browser.get(pub)
-                    try:
-                        nb_likes = browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/div/button/span').text
-                        typ.append("Photo")
+        try:
+            cap = browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/div/ul/div/li/div/div/div[2]/span').text
+        except:
+            cap = "No caption"
 
-                    except: 
-                        nb_likes= browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/section[2]/div/span/span').text
-                        typ.append("Video")
-            try:
-                cap = browser.find_element_by_xpath('html/body/div/section/main/div/div/article/div[3]/div/ul/div/li/div/div/div[2]/span').text
-            except:
-                cap = "No caption"
+        try:
+            c_1 = browser.find_element_by_xpath("//*[contains(text(),'edge_media_to_parent_comment')]")
+            c = c_1.get_attribute('text')
+            co = search(':{"count":{nb},"page_info":',c)
+            nb_co = int(co['nb'])
+        except:
+            nb_co = "No comment"
+        
+        l.append(pub)
+        caption.append(cap)
+        number_of_com.append(nb_co)
+        number_of_lik.append(nb_likes)
+        dates.append(date)
 
-            try:
-                c_1 = browser.find_element_by_xpath("//*[contains(text(),'edge_media_to_parent_comment')]")
-                c = c_1.get_attribute('text')
-                co = search(':{"count":{nb},"page_info":',c)
-                nb_co = int(co['nb'])
-            except:
-                nb_co = "No comment"
-            
-            l.append(pub)
-            caption.append(cap)
-            number_of_com.append(nb_co)
-            number_of_lik.append(nb_likes)
-            dates.append(date)
+    from pandas import DataFrame
 
-        from pandas import DataFrame
+    Influencers = {'Links to publication' : l,
+                'Post description': caption,
+                'Post number of comments': number_of_com,
+                'Post number of likes' : number_of_lik,
+                'Posting date': dates,
+                'Media Type': typ          
+                }
 
-        Influencers = {'Links to publication' : l,
-                    'Post description': caption,
-                    'Post number of comments': number_of_com,
-                    'Post number of likes' : number_of_lik,
-                    'Posting date': dates,
-                    'Media Type': typ          
-                    }
-
-        df = DataFrame(Influencers, columns= ['Links to publication',
-                                        'Post description','Post number of comments','Post number of likes',
-                                            'Posting date','Media Type'])
-        return df
+    df = DataFrame(Influencers, columns= ['Links to publication',
+                                    'Post description','Post number of comments','Post number of likes',
+                                        'Posting date','Media Type'])
+    browser.quit()
+    return df
 
     
